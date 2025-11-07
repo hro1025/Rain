037288051b16f9cddc438d9674ff61bb60ffed55
hro1025 <hro102025@gmail.com>
2025-11-07 10:49:07 +0100

    Fixed the warning

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿namespace Rain;
+﻿using System.Diagnostics;
+using Spectre.Console.Cli;
+
+namespace Rain;
 
 class Program
 {
@@ -9,13 +12,12 @@ class Program
             // This gives output by the program and a input by the user
             Console.WriteLine("Hei!\nRegner det for deg? (Ja eller Nei)");
             string? input = Console.ReadLine();
-
-            if (input.ToLower() == "ja")
+            if (input == "ja" || input == "Ja")
             {
                 Console.WriteLine("Det regner så du bør ta med deg regnjakke og paraply");
                 break; // break makes the loop break and stop the application
             }
-            else if (input.ToLower() == "nei")
+            else if (input == "nei" || input == "Nei")
             {
                 Console.WriteLine("Det regner ikke så du trenger ikke å ta med noe");
                 break;
